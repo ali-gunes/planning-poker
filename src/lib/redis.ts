e148ce6b5fa69c18617ecac04a9ad066bf9f68b0
Ali Gunes <agunes372@gmail.com>
2025-06-12 13:24:06 +0300

    Adjust redis configuration

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,10 +1,30 @@
 import Redis from "ioredis";
 
+// Use a global variable to cache the connection
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var redis: Redis | undefined;
+}
+
+let redis: Redis;
+
 const getRedisUrl = () => {
     if (process.env.REDIS_URL) {
         return process.env.REDIS_URL;
     }
-    throw new Error("REDIS_URL is not defined");
+    throw new Error("REDIS_URL is not defined in the environment");
 }
 
-export const redis = new Redis(getRedisUrl()); 
+if (process.env.NODE_ENV === 'production') {
+  redis = new Redis(getRedisUrl());
+} else {
+  // In development, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  if (!global.redis) {
+    global.redis = new Redis(getRedisUrl());
+  }
+  redis = global.redis;
+}
+
+export { redis }; 
