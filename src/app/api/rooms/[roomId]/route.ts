41dc3789d36bafe806e1849ef5403897686a6536
Ali Gunes <agunes372@gmail.com>
2025-06-12 13:04:27 +0300

    Add explicit return type

--- src/app/api/rooms/[roomId]/route.ts
+++ src/app/api/rooms/[roomId]/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET(
     req: Request,
     { params }: { params: { roomId: string } }
-) {
+): Promise<NextResponse> {
     try {
         const { roomId } = params;
 
