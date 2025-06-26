d11bcf801272ef3e781b94416ee9d6832b36c8cb
Ali Gunes <agunes372@gmail.com>
2025-06-26 12:35:00 +0300

    Add intermediate values for hours

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -12,9 +12,9 @@ import { KubilayModal } from "@/components/KubilayModal";
 import { RoomSettingsModal, type RoomSettingsUpdate } from "@/components/RoomSettingsModal";
 
 const votingStacks = {
-    fibonacci: [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89],
+    fibonacci: [1, 2, 3, 5, 8, 13, 21, 34, 55, 89],
     days: [1, 2, 3, 4, 5, 6, 7, 8],
-    hours: [4, 8, 12, 16, 24, 32, 40, 48, 56, 64],
+    hours: [4, 6, 8, 12, 16, 20, 24, 32, 36, 40, 48, 56, 64],
     yesno: ["Evet", "Hayır"]
 };
 
